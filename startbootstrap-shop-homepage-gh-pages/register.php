f36b7a2b98f8b99f2463f83cd7fd12c61f0a0768
Allison Valdez <asnipes1@jh.edu>
2025-04-18 02:20:43 -0700

    register.php revamped

--- startbootstrap-shop-homepage-gh-pages/register.php
+++ startbootstrap-shop-homepage-gh-pages/register.php
@@ -1,3 +1,55 @@
+<?php
+
+require_once "config.php";
+require_once "session.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+    $fullname = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $password = trim($_POST["password"]);
+    $confirm_password = trim($_POST["confirm_password"]);
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+    if ($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+        $error = "";
+
+        // Bind the parameters (s = string, i = int, b = blob, etc)
+        $query->bind_param("s", $email);
+        $query->execute();
+
+        // store the results to check if the account already exists in the database
+        $query->store_result();
+        if ($query->num_rows > 0) {
+            $error .= "<p class='error'>The email address is already registered!</p>";
+        } else {
+            // proceeed to a creating a new user and validate their password
+            if (strlen($password) < 6) {
+                $error .= "<p class='error'>Password must have atleast 6 characters.";
+            }
+
+            // validate confirm password
+            if (empty($confirm_password)) {
+                $error .= "<p class='error'>Please enter confirm password.</p>";
+            } else {
+                if (empty($error) && ($password != $confirm_password)) {
+                    $error .= "<p class='error'>Password did not match.</p>";
+                }
+            }
+
+            if (empty($error)) {
+
+            } else {
+                $error .= "<p class='error'>Something went wrong!</p>";
+            }
+        }
+    }
+}
+$query->close();
+$insertQuery->close();
+// Close DB connection
+mysqli_close($db);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
