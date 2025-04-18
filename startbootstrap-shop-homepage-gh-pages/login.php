580e89cda0e9134eb10bd091c7b30ed1c5181fbb
Allison Valdez <asnipes1@jh.edu>
2025-04-18 03:08:20 -0700

    login.php revamped

---
+++ startbootstrap-shop-homepage-gh-pages/login.php
@@ -0,0 +1,131 @@
+<?php
+
+require_once "config.php";
+require_once "session.php";
+
+$error = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+    $email = trim($_POST["email"]);
+    $password = trim($_POST["password"]);
+
+    // Validate if email is empty
+    if (empty($email)){
+        $error .= "<p class='error'>Please enter email.</p>";
+    }
+
+    // Validate if password is empty
+    if (empty($password)){
+        $error .= "<p class='error'>Please enter your password.</p>";
+    }
+    if (empty($error)){
+        if ($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+            $query->bind_param("s", $email);
+            $query->execute();
+            $row = $query->fetch();
+
+            if ($row){
+                if (password_verify($password, $row["password"])){
+                    $_SESSION["userid"] = $row["id"];
+                    $_SESSION["user"] = $row;
+
+                    // Redirect proper users to the welcome page
+                    header("location: welcome.php");
+                    exit;
+                } else {
+                    $error .= "<p class='error'>The password is not valid.";
+                }
+            } else {
+                $error .= "<p class='error'>No User exist with that email address.</p>";
+            }
+        }
+        $query-> close();
+    } 
+    // Close the connection
+    mysqli_close($db);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Log In</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link
+        href="https://urldefense.proofpoint.com/v2/url?u=https-3A__cdn.jsdelivr.net_npm_bootstrap-2Dicons-401.5.0_font_bootstrap-2Dicons.css&d=DwIFAg&c=qgVugHHq3rzouXkEXdxBNQ&r=luG00nudVsROYkGFlrzv1wBwclTx3tjYar6JDPQLUDo&m=_NF54L6mvmzJX_Ro7O4Ljb1SvTNu58-m0GdKQ0odfm4xenjs8h3zlKzlnfOlQn4g&s=0mvxfy6gyz9q2G2-4zbNYmEPBGRtkOrk3AeZk9bWeoE&e= "
+        rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body class="text-center">
+    <div class="row">
+        <!-- Section-->
+        <section class="py-5 vh-100">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
+                    <div class="py-5 h-100">
+                        <main class="form-signin">
+                            <img class="mb-3" src="./assets/icon.jpg" alt="" width="300" height="350">
+
+                            <h1 class="h3 mb-3 fw-normal">Log In</h1>
+                            <p> Enter your username and password</p>
+
+                            <form action="" method="post">
+
+                                <div class="mb-3 form-floating form-group">
+                                    <label>Email Address</label>
+                                    <input type="email" name="email" class="form-control" placeholder="email@name.com"
+                                        required>
+                                </div>
+
+                                <div class="mb-3 form-floating form-group">
+                                    <label>Password</label>
+                                    <input type="password" name="password" class="form-control"
+                                        placeholder="************" required>
+                                </div>
+
+                                <div class="mb-3 form-floating form-group">
+                                    <input type="submit" name="submit"
+                                        class="w-100 btn btn-lg btn-primary bg-dark text-white" value="submit">
+                                </div>
+
+                                <p>Create an account
+                                    <a class="btn-danger link-dark" href="login.php">Here</a>
+                                </p>
+                            </form>
+                    </div>
+                </div>
+            </div>
+            </main>
+
+    </div>
+    </div>
+    </div>
+    </section>
+    </div>
+    </div>
+    </section>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Mayar's Meat Market &copy; 2025</p>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script
+        src="https://urldefense.proofpoint.com/v2/url?u=https-3A__cdn.jsdelivr.net_npm_bootstrap-405.2.3_dist_js_bootstrap.bundle.min.js&d=DwIFAg&c=qgVugHHq3rzouXkEXdxBNQ&r=luG00nudVsROYkGFlrzv1wBwclTx3tjYar6JDPQLUDo&m=_NF54L6mvmzJX_Ro7O4Ljb1SvTNu58-m0GdKQ0odfm4xenjs8h3zlKzlnfOlQn4g&s=SASoPfqBWSzPUFDHDxfJHFAOVRjalvnt6v6qS5WNKuE&e= "></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    </div>
+</body>
+
+</html>
