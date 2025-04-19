8e6403a8a2dff635dc7a7731b7b86e30c430da68
Allison Valdez <asnipes1@jh.edu>
2025-04-18 22:03:27 -0700

    trying to fix php

--- users.sql
+++ users.sql
@@ -1,8 +1,10 @@
-CREATE DATABASE user_signup;
+CREATE DATABASE user_registration;
 
-CREATE TABLE users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE
-);
+CREATE TABLE `users` (
+	`id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(75) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci auto_increment=1;
